f53ade433d351b8db6b224ef19b1875c01bc1f15
Aishwary <aishwarypatle@26gmail.com>
2024-03-09 22:14:30 +0530

    patterns 21 n*n mattrix addded in js

--- Patterns/Pattern21.cpp
+++ Patterns/Pattern21.cpp
@@ -3,31 +3,41 @@
 using namespace std;
 
 void pattern(int n){
-    for(int i=0;i<n;i++){
-        cout << "*" ;
+    // for(int i=0;i<n;i++){
+    //     cout << "*" ;
         
-    }
-    cout << endl;
-    for(int i=0;i<n-2;i++){
-        for (int j = 0; j < 1; j++)
-        {
-            cout << "*" ;
-        }
-        for (int j = 0; j < n-2; j++)
-        {
-            cout << " " ;
-        }
-        for (int j = 0; j < 1; j++)
-        {
-            cout << "*" ;
-        }
+    // }
+    // cout << endl;
+    // for(int i=0;i<n-2;i++){
+    //     for (int j = 0; j < 1; j++)
+    //     {
+    //         cout << "*" ;
+    //     }
+    //     for (int j = 0; j < n-2; j++)
+    //     {
+    //         cout << " " ;
+    //     }
+    //     for (int j = 0; j < 1; j++)
+    //     {
+    //         cout << "*" ;
+    //     }
         
-        cout << endl;
-    }
-    for(int i=0;i<n;i++){
-        cout << "*" ;
+    //     cout << endl;
+    // }
+    // for(int i=0;i<n;i++){
+    //     cout << "*" ;
         
+    // }
+     cout << string(n, '*') << endl;
+
+    // Middle rows
+    string middleRow = "*" + string(n - 2, ' ') + "*";
+    for (int i = 0; i < n - 2; i++) {
+        cout << middleRow << endl;
     }
+
+    // Last row
+    cout << string(n, '*') << endl;
 }
 
 // Input: ‘N’ = 4
